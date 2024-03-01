9d96ecb258e73518df4688eefd19ecf0f6172643
Md. Rakibul Asib Redoy <126803555+Asib177@users.noreply.github.com>
2024-03-01 23:53:26 +0600

    Add files via upload

---
+++ calculation.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int b = 10;
+    printf("%d\n", b + b * (++b));
+
+    return 0;
+}
---
+++ incrementnumber.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = 19;
+    ++a;
+    printf("%d", a);
+    
+    return 0;
+}
